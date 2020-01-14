d73bf0b748e0256fc97a69c39d39abdcbf9f9680
Alophes <bryan.gasse@gmail.com>
2020-01-14 17:26:44 -0500

    Base Game

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target;
+    public float deadlinesOnTarget;
+    public Vector2 maxPositionMap; //(6.25, 14)
+    public Vector2 minPositionMap; //(-20.35, -2)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (!IsSamePosition()) Move();
+    }
+
+    private void Move()
+    {
+        Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+        targetPosition = Bounding(targetPosition);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, deadlinesOnTarget);
+    }
+
+    private Vector3 Bounding(Vector3 position)
+    {
+        position.x = Mathf.Clamp(position.x, minPositionMap.x, maxPositionMap.x);
+        position.y = Mathf.Clamp(position.y, minPositionMap.y, maxPositionMap.y);
+        return position;
+    }
+
+    private bool IsSamePosition()
+    {
+        return transform.position == target.position;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState
+{
+    Walk,
+    Attack,
+    Interact
+}
+
+public class PlayerMovement : MonoBehaviour
+{
+    public PlayerState currentState;
+    public float speed;
+    private Rigidbody2D body2D;
+    private Vector3 position;
+    private Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = PlayerState.Walk;
+        animator = GetComponent<Animator>();
+        animator.SetFloat("MoveX", 0);
+        animator.SetFloat("MoveY", -1);
+        body2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMovement();
+    }
+
+    void UpdateMovement()
+    {
+        UpdatePosition();
+        if (Input.GetButtonDown("Attack") && currentState != PlayerState.Attack) Attack();
+        else if (currentState == PlayerState.Walk) Move();
+    }
+
+    private void Attack()
+    {
+        StartCoroutine(Routine(.3f,
+            () =>
+            {
+                animator.SetBool("Attacking", true);
+                currentState = PlayerState.Attack;
+            },
+            () =>
+            {
+                animator.SetBool("Attacking", false);
+            },
+            () =>
+            {
+                currentState = PlayerState.Walk;
+            }
+        ));
+    }
+
+    private IEnumerator Routine(float waitTime, Action action, Action reset, Action after)
+    {
+        action();
+        yield return null;
+        reset();
+        yield return new WaitForSeconds(waitTime);
+        after();
+    }
+
+    private void Move()
+    { 
+        if (IsMoving())
+        {
+            position.Normalize();
+            body2D.MovePosition( transform.position + position * speed * Time.deltaTime );
+            animator.SetFloat("MoveX", position.x);
+            animator.SetFloat("MoveY", position.y);
+            animator.SetBool("Moving", true);
+        }
+        else
+        {
+            animator.SetBool("Moving", false);
+        }
+    }
+
+    private void UpdatePosition()
+    {
+        position = Vector3.zero;
+        position.x = Input.GetAxisRaw("Horizontal");
+        position.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private bool IsMoving()
+    {
+        return position != Vector3.zero;
+    }
+
+    
+}
