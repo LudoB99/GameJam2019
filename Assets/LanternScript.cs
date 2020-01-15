b68efb0e43aeb9c5af23b32c99a4a6cedb4053fa
TimF19 <timothy.farley@hotmail.com>
2020-01-15 16:34:11 -0500

    oil

--- Assets/LanternScript.cs
+++ Assets/LanternScript.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class LanternScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float OilQuantity;
+    public Light Light;
+    public Collider2D Huge;
+    public Collider2D Mid;
+    public Collider2D Little;
+    
     void Start()
     {
-        
+        InvokeRepeating("ConsumeOil", 2.0f, 1.0f);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Light.range = OilQuantity;
+
+        if (OilQuantity < 0)
+        {
+            Little.enabled = false;
+        }
+        else
+        {
+            if (OilQuantity < 5)
+            {
+                Mid.enabled = false;
+            }
+            else
+            {
+                if (OilQuantity < 10)
+                {
+                    Huge.enabled = false;
+                }
+            }
+        }
+    }
+
+    private void ConsumeOil()
+    {
+        OilQuantity -= 0.66f;
+    }
+    
+    public void RefillOil()
+    {
+        OilQuantity = 10;
+        Huge.enabled = true;
+        Mid.enabled = true;
+        Little.enabled = true;
     }
 }
