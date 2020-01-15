757a2d7c862c353534ee9fb44581b6ec3f40e507
guillaumedufour <guillaumedufour98@gmail.com>
2020-01-14 23:27:42 -0500

    Je vous aimes
    Je v ous aimes

---
+++ Assets/BarrelScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelScript : MonoBehaviour
+{ 
+
+    public Player Player;
+    private bool playerTouchingBarrel;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Collect") && playerTouchingBarrel)
+        {
+            Player.RefillOil();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        if (hitInfo.CompareTag("Player"))
+        {
+            playerTouchingBarrel = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D hitInfo)
+    {
+        if (hitInfo.CompareTag("Player"))
+        {
+            playerTouchingBarrel = false;
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    public float OilQuantity;
 
     // Start is called before the first frame update
     void Start()
     {
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RefillOil()
     {
+        OilQuantity = 100;
     }
 }
