1adec2afcfa262960a60eaa07302565621bf1f0a
saynnn <39345951+saynnn@users.noreply.github.com>
2020-01-15 17:27:12 -0500

    Title fade scene

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -2,15 +2,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Image black;
+    public Animator anim;
+    
     private void Update()
     {
         if (Input.GetButtonDown("Submit"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            StartCoroutine(Fading());
         }
+    }
+
+    IEnumerator Fading()
+    {
+        anim.SetBool("fade", true);
+        yield return new WaitUntil(() => black.color.a == 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
