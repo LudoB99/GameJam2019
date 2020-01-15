6c97aac08dbeb4364d113ab95bfd5a4f957b145a
saynnn <39345951+saynnn@users.noreply.github.com>
2020-01-14 21:01:23 -0500

    Fix Bryan canvas

--- Assets/Scripts/PlayerHit.cs
+++ Assets/Scripts/PlayerHit.cs
@@ -14,7 +14,7 @@ public class PlayerHit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
