3cda3982569b3a703cc0159c45cd3753f4aa37b3
guillaumedufour <guillaumedufour98@gmail.com>
2020-01-15 10:10:27 -0500

    Système de signal
    Système de signal

---
+++ Assets/Scripts/Clue.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clue : MonoBehaviour
+{
+
+    public GameObject clue;
+
+    public void OnEnable()
+    {
+        clue.SetActive(true);
+    }
+
+    public void Disable()
+    {
+        clue.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
