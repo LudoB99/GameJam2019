d2aab16fd7d002755067bb4f7cb5f7e92debd687
guillaumedufour <guillaumedufour98@gmail.com>
2020-01-15 20:18:59 -0500

    Items de l'inventaire

---
+++ Assets/InventoryItemScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryItemScript : Interactable
+{
+    public override void Interact()
+    {
+        if (Input.GetButtonDown("Collect") && playerInRange)
+        {
+            if (DialogBox.activeInHierarchy)
+            {
+                DialogBox.SetActive(false);
+            }
+            else
+            {
+                DialogBox.SetActive(true);
+                DialogText.text = Dialog;
+            }
+        }
+    }
+}
